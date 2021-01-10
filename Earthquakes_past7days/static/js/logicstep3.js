7d37682d1f663646d49e84fcb947fd33b74dd7af
ccastanette <ccastanette@gmail.com>
2021-01-09 16:55:06 -0800

    adding magnitude colors & popups

--- Earthquakes_past7days/static/js/logicstep3.js
+++ Earthquakes_past7days/static/js/logicstep3.js
@@ -36,7 +36,11 @@ pointToLayer: function(feature, latlng) {
     console.log(data);
     return L.circleMarker(latlng);
     },
-    style:styleInfo
+    style:styleInfo,
+    //creating popups
+    onEachFeature: function(feature, layer){
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
   }).addTo(map);
 
 // This function returns the style data for each of the earthquakes we plot on
@@ -46,13 +50,32 @@ function styleInfo(feature) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
   }
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude >5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
